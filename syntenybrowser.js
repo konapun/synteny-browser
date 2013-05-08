393fa6c4d5afa6f52bc61e7adc7a2c5dde5461c9
Bremen Braun <bremen.braun@gmail.com>
2013-05-08 17:35:14 -0600

    Update syntenybrowser.js

--- syntenybrowser.js
+++ syntenybrowser.js
@@ -16,6 +16,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 var SyntenyBrowser = function(div, opts) {
 	var canvas = document.createElement('canvas');
 	div.appendChild(canvas);
